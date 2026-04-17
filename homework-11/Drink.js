a2f82b226c6109933585edade2a5b13764ddd64b
CEM <cem@karimzoda.local>
2026-04-17 23:19:35 +0200

    Working with classes

---
+++ homework-11/Drink.js
@@ -0,0 +1,35 @@
+export class Drink {
+    #temperature;
+    constructor(name, size, price, temperature) {
+        this.name = name;
+        this.size = size;
+        this.price= price;
+        this.#temperature = temperature;
+    }
+
+    getInfoDrink() {
+        return (`Напиток:${this.name}, Размер:${this.size}, Цена:${this.price}`);
+    }
+
+    getTemperatureDrink() {
+        return (`Температура: ${this.#temperature}`);
+    }
+
+    setTemperatureDrink(val) {
+        this.#temperature = val
+        if (this.#temperature > 90) {
+            console.log("Температура превышенна");
+        } else {
+            console.log("Температура стабльна :)");
+        }
+    }
+
+    #prepareDrink () {
+        console.log(`Напиток ${this.name} готовится`);
+    }
+
+    serverDrink () {
+        this.#prepareDrink() 
+        console.log(`Напиток ${this.name} подан! Температура: ${this.#temperature}`);
+    }
+};
---
+++ homework-11/Juice.js
@@ -0,0 +1,13 @@
+import { Drink } from './Drink.js'; 
+
+export class Juice extends Drink {
+  constructor(name, size, price, temperature, fruitType){
+    super(name, size, price, temperature);
+    this.fruitType = fruitType;
+  }
+
+  getInfoDrink() {
+    const fruitsInfo = super.getInfoDrink();
+    return (`Инфо: ${fruitsInfo}, Фрукт: ${this.fruitType}`);
+  }
+};
